c90d79f775b6209b82f74ac3ed5298c11d5a1bb3
minyklc <suizumina@gmail.com>
2026-02-12 16:25:26 +0100

    test ui

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,8 +1,35 @@
 #!/usr/bin/env python3
+import sys
+
+
+def interaction():
+    print('1 = regenerate new maze')
+    print('2 = show/hide the shortest path')
+    print('3 = change wall colours')
+    print('4 = play the maze')
+    print('q = quit')
+    print()
 
 
 def main():
     
+    interaction()
+    for line in sys.stdin:
+        interaction()
+        if line.rstrip() == 'q':
+            break
+        elif line.rstrip() == '1':
+            ...
+        elif line.rstrip() == '2':
+            ...
+        elif line.rstrip() == '3':
+            ...
+        elif line.rstrip() == '4':
+            ...
+        else :
+            print('please select another key...')
+    
+    print("Exit")
 
 
 if __name__ == '__main__':
---
+++ maze.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+from rich.console import Console
+
+
+def print_maze(maze):
+    for n in maze:
+        print(" ".join(n))
+    print()
+
+
+def maze():
+    maze = [
+        [2, 11, 6],
+        [3, 7, 13],
+        [6, 10, 15],
+        [12, 7, 9],
+    ]
+    # print_maze(maze)
+    for i in range(len(maze)):
+        for j in range(len(maze[0])):
+            if maze[i][j] < 10:
+                maze[i][j] = int(hex(maze[i][j]).split('x')[-1])
+            else:
+                maze[i][j] = hex(maze[i][j]).split('x')[-1].capitalize()
+    # print_maze(maze)
+
+    c = Console()
+    c.print("┏━┳━┓")
+    c.print("║   ║")
+    c.print("╚═══╝")
+    print()
+    print("┏━┳━┓")
+    print("┣━╋━┫")
+    print("┗━┻━┛")
+    maze2 = [
+        ['┏', '━', '┳', '━', '┓'],
+        ['┣', '╋', '┫'],
+        ['┗', '┻', '┛'],
+    ]
+    print_maze(maze2)
+
+
+if __name__ == "__main__":
+    maze()
