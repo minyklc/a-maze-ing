a51350092be80ce9013ca7c159c1465ff5782c51
minyklc <suizumina@gmail.com>
2026-02-17 02:08:07 +0100

    generate

--- generator.py
+++ generator.py
@@ -4,36 +4,68 @@
 class Box:
     def __init__(self, x: int, y: int):
         self.pos = (x, y)
-        self.walls = [1, 1, 0, 0]
-    
-    def add_wall(self, dir):
-        ...
-    
-    def remove_wall(self, dir):
-        ...
+        self.walls = 0b1111
+
+    def add_wall(self, dir) -> None:
+        if dir == 'north':
+            self.walls += 0b0001
+        elif dir == 'east':
+            self.walls += 0b0010
+        elif dir == 'south':
+            self.walls += 0b0100
+        elif dir == 'west':
+            self.walls += 0b1000
+
+    def remove_wall(self, dir) -> None:
+        if dir == 'north':
+            self.walls -= 0b0001
+        elif dir == 'east':
+            self.walls -= 0b0010
+        elif dir == 'south':
+            self.walls -= 0b0100
+        elif dir == 'west':
+            self.walls -= 0b1000
 
 
 class Maze:
-    def __init__(self, width: int, length: int, perfect: bool):
+    def __init__(self, width: int, length: int,
+                 start: tuple, end: tuple, perfect: bool):
         # self.w = width
         # self.l = length
         self.p = perfect
+        self.s = start
+        self.e = end
         self.m = self.blank_maze(width, length)
-    
+        
+
     def blank_maze(self, width: int, length: int) -> list[list[int]]:
         maze = []
-        for n in range(width + 1):
+        for n in range(width):
             maze.append([])
-            for b in range(length + 1):
+            for b in range(length):
                 maze[n].append(Box(b, n))
         return maze
+
+    def generate(self):
+        if self.p is True:
+            self.perfect_maze()
+        else:
+            self.imperfect_maze()
+
+    def perfect_maze(self):
+        # stack = []
+        ...
+
+    def imperfect_maze(self):
+        ...
 
 
 def print_maze(maze):
     for n in maze:
-        print(n)
-        # for b in n:
-        #     print(b.pos)
+        # print(n)
+        for b in n:
+            print(b.walls, end=' ')
+        print()
     print()
 
 
@@ -43,12 +75,14 @@ def generator():
     #     [5, 3, 10],
     #     [13, 4, 6],
     # ]
-    exit = [0, 1] # x, y
-    pos = [0, 0]
+    end = (0, 1) # x, y
+    start = (0, 0)
     width = 3
     length = 3
-    maze = Maze(width, length, True)
+    maze = Maze(width, length, start, end, True)
+    maze.generate()
     print_maze(maze.m)
+    maze.p
 
 
 if __name__ == "__main__":
