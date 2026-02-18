53304c3e43e17d876e19150a508288c8068bffa7
minyklc <suizumina@gmail.com>
2026-02-18 15:20:51 +0100

    perfect not so perfect

--- generator.py
+++ generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from random import random, randint
+from random import random, randint, choice
 
 
 class Box:
@@ -49,7 +49,6 @@ class Maze:
         self.s = start
         self.e = end
         self.m = self.blank_maze(width, length)
-        
 
     def blank_maze(self, width: int, length: int) -> list[list[Box]]:
         maze = []
@@ -67,11 +66,66 @@ class Maze:
 
     def perfect_maze(self):
         stack = []
-        pos = (randint(0, self.l), randint(0, self.w))
-        while len(stack) < self.l * self.w:
-            ...
-        
+        initial = (randint(0, self.l - 1), randint(0, self.w - 1))
+        pos = [initial[0], initial[1]]
+        stack.append(pos) # stack of lists of position
+        while pos:
+            # temp = (pos[0], pos[1])
+            try:
+                next = choice(self.can_pass_through(pos, stack))
+            # if next:
+                self.m[pos[1]][pos[0]].remove_wall(next)
+                pos = self.update_pos(pos, next)
+                stack.append(pos)
+            except IndexError:
+                stack.pop(-1)
+                pos = stack[-1]
+            if self.is_same(pos, initial):
+                pos = []
+                
+    def update_pos(self, pos: list, dir: str) -> list:
+        if dir == 'N':
+            pos[1] -= 1
+        elif dir == 'E':
+            pos[0] += 1
+        elif dir == 'S':
+            pos[1] += 1
+        elif dir == 'W':
+            pos[0] -= 1
+        return pos
 
+    def can_pass_through(self, pos: list, stack: list) -> list:
+        r = []
+        n = []
+        if pos[1] > 0:
+            n = self.m[pos[1] - 1][pos[0]].pos
+        e = []
+        if pos[0] < self.l - 1:
+            e = self.m[pos[1]][pos[0] + 1].pos
+        s = []
+        if pos[1] < self.w - 1:
+            s = self.m[pos[1] + 1][pos[0]].pos
+        w = []
+        if pos[0] > 0:
+            w = self.m[pos[1]][pos[0] - 1].pos
+
+        if n and not any(p for p in stack if self.is_same(p, n)):
+            r.append('N')
+        if e and not any(p for p in stack if self.is_same(p, e)):
+            r.append('E')
+        if s and not any(p for p in stack if self.is_same(p, s)):
+            r.append('S')
+        if w and not any(p for p in stack if self.is_same(p, w)):
+            r.append('W')
+
+        return r
+
+    @staticmethod
+    def is_same(pos: list, pos2: tuple) -> bool:
+        if pos[0] == pos2[0] and pos[1] == pos2[1]:
+            return True
+        return False
+        
     def imperfect_maze(self):
         ...
 
@@ -98,11 +152,8 @@ def generator():
     maze = Maze(width, length, start, end, True)
     maze.generate()
     print_maze(maze.m)
-    maze.p
-    maze.m[0][2].remove_wall(['W', 'N', 'E', 'S'])
-    maze.m[0][2].add_wall(['W', 'E', 'S'])
-    print(maze.m[0][2].not_wall())
     maze.generate()
+    print_maze(maze.m)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -4,3 +4,11 @@ n = 0b1111
 r = 0b1111 / 2
 r = int(r)
 print(r)
+
+pos = 0
+if pos:
+    print(0)
+
+pos = (0, 1)
+next_pos = [0, 1]
+print(pos[0] == next_pos[0], pos[1] == next_pos[1])
